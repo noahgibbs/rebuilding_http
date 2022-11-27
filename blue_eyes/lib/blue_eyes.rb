89971ecb3e08bf1e3387ad7dce7b0466fb02619b
Noah Gibbs <the.codefolio.guy@gmail.com>
2022-11-27 13:44:10 +0000

    First BlueEyes framework code

--- blue_eyes/lib/blue_eyes.rb
+++ blue_eyes/lib/blue_eyes.rb
@@ -1,8 +1,67 @@
 # frozen_string_literal: true
-
+require 'socket'
 require_relative "blue_eyes/version"
 
 module BlueEyes
   class Error < StandardError; end
-  # Your code goes here...
+  class ParseError < Error; end
+end
+
+class BlueEyes::Request
+  attr_reader :url
+
+  def initialize(s)
+    parse_req s.gets
+
+    head = String.new
+    loop do
+      line = s.gets
+      head << line.chomp("\r").chomp("\n") << "\n"
+      break if line.strip == ""
+    end
+
+    parse_headers(head)
+  end
+
+  def parse_req(line)
+    @method, @url, rest = line.split(/\s/, 3)
+    if rest =~ /HTTP\/(\d+)\.(\d+)/
+      @http_version = "#{$1}.#{$2}"
+    else
+      pe = BlueEyes::ParseError
+      raise pe.new("Can't parse request")
+    end
+  end
+
+  def parse_headers(text)
+    @headers = {}
+    text.lines.each do |line|
+      break if line.strip.empty?
+      field, value = line.split(":", 2)
+      @headers[field.downcase] = value.strip
+    end
+  end
+end
+
+class BlueEyes::Response
+  def initialize(body,
+      version: "1.1",
+      status: 200,
+      message: "OK",
+      headers: {})
+    @version = version
+    @status = status
+    @message = message
+    @headers = headers
+    @body = body
+  end
+
+  def to_s
+    <<RESPONSE
+HTTP/#{@version} #{@status} #{@message}\r
+#{@headers.map {|k,v| "#{k}: #{v}" }.join("\r\n")}
+\r
+#{@body}
+RESPONSE
+  end
 end
---
+++ blue_eyes/test_server.rb
@@ -0,0 +1,12 @@
+# test_server.rb
+require_relative 'lib/blue_eyes'
+
+server = TCPServer.new 4321
+loop do
+  client = server.accept
+  req = BlueEyes::Request.new(client)
+  resp = BlueEyes::Response.new("Hello from Ruby!")
+  client.write resp.to_s
+  client.close
+end
+
