4ca84d60bf717064ce3c6623b0306a16631ac520
Noah Gibbs <the.codefolio.guy@gmail.com>
2022-11-28 23:01:20 +0000

    Thread pool code

--- blue_eyes/lib/blue_eyes/dsl.rb
+++ blue_eyes/lib/blue_eyes/dsl.rb
@@ -7,13 +7,6 @@ require_relative "../blue_eyes"
 include BlueEyes::DSL
 
 at_exit do
-    server = TCPServer.new 4321
-    loop do
-      client = server.accept
-      req = BlueEyes::Request.new(client)
-      resp = RUBY_MAIN.match(req)
-      client.write resp.to_s
-      client.close
-    end
+  s = BlueEyes::Server.new(4321)
+  s.start
 end
-
